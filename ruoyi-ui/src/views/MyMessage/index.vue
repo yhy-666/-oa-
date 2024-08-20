40c1218968cdc7a32a5340cd6479e1abcef3162a
Zion-Lange <singtinyu@163.com>
2024-08-20 11:24:06 +0800

    本次创建了收件箱

---
+++ ruoyi-ui/src/views/MyMessage/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- 使用Element UI的Card组件展示邮件列表 -->
+    <el-row :gutter="20">
+      <el-col :span="8" v-for="email in emails" :key="email.emailID">
+        <el-card
+          @click.native="fetchEmailDetails(email.emailID)"
+        >
+        </el-card>
+      </el-col>
+      <el-col v-if="!emails.length" :span="8">
+        <div class="no-email">没有邮件</div>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
